8d68c5883dddbb8d9b8deefeeb93a3e2c7982d51
Eudis Alves <deleudis@gmail.com>
2024-04-14 20:44:40 -0300

    Finalizando a grade da arena

--- arena.js
+++ arena.js
@@ -14,7 +14,7 @@ export default class Arena {
   draw() {
     this._drawBorder();
     //_drawSquares()
-    //_drawGrid()
+    this._drawGrid()
   }
 
   _drawBorder() {//metodo para desenhar a borda
@@ -25,5 +25,26 @@ export default class Arena {
       this._width,
       this._height
     );
+  }
+
+  _drawGrid() {//Metodod para desenhar a grade dos blocos
+
+    //beginPath: metodo que diz que ira começar a desenhar uma sequencias de linhas
+    //moveTo: metodo diz de onde a linha começar
+    //lineTo: metodo que diz onde a linha vai terminar
+    //storke: metodo para desenhar tudo que foi dito nos metodos moveTo e lineTo
+  
+    GameManager.context.strokeStyle = "#dedede";
+    GameManager.context.beginPath();
+    for (let i = 1; i< this._lines; i++) {
+      GameManager.context.moveTo(this.position.left, this.position.top + GameManager.config.squareSize * i);
+      GameManager.context.lineTo(this.position.left + this._width , this.position.top + GameManager.config.squareSize * i);
+    }
+    for (let i = 1; i< this._columns; i++) {
+      GameManager.context.moveTo(this.position.left + GameManager.config.squareSize * i, this.position.top);
+      GameManager.context.lineTo(this.position.left + GameManager.config.squareSize * i, this.position.top + this._height);
+    }
+    
+    GameManager.context.stroke();
   }
 }
